f05e51b4f25fd0d669e73aa2d6474aa317f24428
DESKTOP-5B1D5HD\snowc <tristan829@gmail.com>
2021-10-26 20:57:11 +0800

    fix bootstrap-react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import BoatHistory from './page/BoatHistory/BoatHistory.jsx'
 
 ReactDOM.render( <App/> , document.querySelector('#root'))
